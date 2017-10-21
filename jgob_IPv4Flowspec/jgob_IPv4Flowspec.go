3e5f41648c49a16fb392d104eb05c93115898ffa
naoya4arale <n4sekai5y@gmail.com>
2017-10-22 03:05:10 +0900

    3rd

--- jgob_IPv4Flowspec/jgob_IPv4Flowspec.go
+++ jgob_IPv4Flowspec/jgob_IPv4Flowspec.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
-	"encoding/json"
 	"fmt"
+	"encoding/json"
+	"github.com/joho/godotenv"
 	"io"
-	"io/ioutil"
+	//	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
 )
-
-type Announce struct {
-	ORIGIN  uint8    `json:"origin"`  //bgp origin-as attr.
-	NEXTHOP string   `json:"nexthop"` //bgp nexthop attr.
-	ASPATH  []uint32 `json:"aspath"`  //bgp as-path attr.
-	SRC_IP  string   `json:"src-ip"`  //bgp announce prefix of address.
-	SRC_CIDR    uint8    `json:"src-cidr"`    //bgp announce prefix of cidr.
-	DST_IP string
-	DST_CIDR    uint8
-	SRC_PORT 
-}
 
 func Env_load() {
 	err := godotenv.Load()
@@ -34,24 +23,24 @@ func Env_load() {
 
 func main() {
 
-	a := make(chan Announce)
+	a := make(chan string)
 
 	go JgobServer(a)
 
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		if checkAuth(r) == false {
-			w.Header().Set("WWW-Authenticate", `Basic realm="MY REALM"`)
+			w.Header().Set("WWW-Authenticate", `Basic realm="JGOB REALM"`)
 			w.WriteHeader(401)
 			w.Write([]byte("401 Unauthorized\n"))
 			return
 		} else {
-			w.Write([]byte("API is up and running\n"))
+			w.Write([]byte("JGOB is up and running\n"))
 		}
 	})
 
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
 		if checkAuth(r) == false {
-			w.Header().Set("WWW-Authenticate", `Basic realm="MY REALM"`)
+			w.Header().Set("WWW-Authenticate", `Basic realm="JGOB REALM"`)
 			w.WriteHeader(401)
 			w.Write([]byte("401 Unauthorized\n"))
 			return
@@ -90,46 +79,62 @@ func main() {
 				return
 			}
 
-			var res Announce
-			var resOriginBufUint8 uint8
+			//var res Announce
+			res := "match "
 
-			if jsonBody["origin"] == "igp" {
-				resOriginBufUint8 = 0
-			} else if jsonBody["origin"] == "egp" {
-				resOriginBufUint8 = 1
-			} else if jsonBody["origin"] == "incomplete" {
-				resOriginBufUint8 = 2
-			} else {
-				resOriginBufUint8 = 2
-			}
-			res.ORIGIN = resOriginBufUint8
+			resAry := make([]string ,0 ,20)
 
-			res.NEXTHOP = jsonBody["nexthop"]
-
-			if strings.Contains(jsonBody["aspath"], " ") {
-				resAspathBufAry := strings.Split(jsonBody["aspath"], " ")
-				for _, s := range resAspathBufAry {
-					sInt, err := strconv.Atoi(s)
-					if err != nil {
-						fmt.Printf("error5: %s\n", err)
-					}
-					res.ASPATH = append(res.ASPATH, uint32(sInt))
-				}
-			} else {
-				resAspathBufInt, err := strconv.Atoi(jsonBody["aspath"])
-				if err != nil {
-					fmt.Printf("error6: %s\n", err)
-				}
-				res.ASPATH = append(res.ASPATH, uint32(resAspathBufInt))
+			if jsonBody["proto"] == "tcp" || jsonBody["proto"] == "udp" || jsonBody["proto"] == "icmp"  {
+				s := "protocol " + jsonBody["proto"]
+				resAry = append(resAry, s)
 			}
 
-			res.ADDRESS = jsonBody["address"]
-
-			resCidrBufInt, err := strconv.Atoi(jsonBody["cidr"])
-			if err != nil {
-				fmt.Printf("error1: %s\n", err)
+			if jsonBody["src-ip"] != "" {
+				s := "source "  + jsonBody["src-ip"]
+				resAry = append(resAry, s)
 			}
-			res.CIDR = uint8(resCidrBufInt)
+
+			if jsonBody["dst-ip"] != "" {
+				s := "destination "  + jsonBody["dst-ip"]
+				resAry = append(resAry, s)
+			}
+
+			if jsonBody["src-port"] != "" {
+				s := "source-port " + `==` + jsonBody["src-port"] + ""
+				resAry = append(resAry, s)
+			}
+
+			if jsonBody["dst-port"] != "" {
+                                s := "destination-port " + `==` + jsonBody["dst-port"] + ""
+                                resAry = append(resAry, s)
+                        }
+
+			if jsonBody["action"] == "accept" ||  jsonBody["action"] == "discard"  {
+				s:= "then " + jsonBody["action"]
+				resAry = append(resAry, s)
+			} else if jsonBody["rate-limit"] != "" {
+				s:= "then rate-limit " + jsonBody["rate-limit"]
+				resAry = append(resAry, s)
+			}
+
+                        if jsonBody["origin"] == "igp" || jsonBody["origin"] == "egp" || jsonBody["origin"] == "incomplete" {
+                                s := "origin " + jsonBody["origin"]
+                                resAry = append(resAry, s)
+                        }
+
+                        if jsonBody["aspath"] != "" {
+                                s := "aspath " + jsonBody["aspath"]
+                                resAry = append(resAry, s)
+                        }
+
+                        if jsonBody["commuinities"] != "" {
+                                s := "community " + jsonBody["community"]
+                                resAry = append(resAry, s)
+                        }
+
+			res = res + strings.Join(resAry, " ")
+
+			fmt.Println(res)
 
 			a <- res
 
@@ -150,4 +155,3 @@ func checkAuth(r *http.Request) bool {
 	}
 	return username == os.Getenv("USERNAME") && password == os.Getenv("PASSWORD")
 }
-

de1f1b2bc1645474950f6b145621f1d3ebd10c03
naoya4arale <n4sekai5y@gmail.com>
2017-11-03 07:49:58 +0900

    change dir

--- test_ssl/makeSSL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-openssl genrsa 2048 > myself.key
-openssl req -new -key myself.key > myself.csr
-openssl x509 -days 3650 -req -signkey myself.key < myself.csr > myself.crt
-mkdir -p ssl/development/
-mv myself.crt ssl/development
-mv myself.csr ssl/development
-mv myself.key ssl/development

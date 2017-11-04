e3ffd720f516e813031b6c5d14e0a927545aa8f9
nao4arale <n4sekai5y@gmail.com>
2017-11-04 13:48:38 +0900

    travice hix

--- jgob_IPv4Flowspec/makeSSL.sh
+++ jgob_IPv4Flowspec/makeSSL.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
 openssl genrsa 2048 > myself.key
-openssl req -new -key myself.key > myself.csr
+#openssl req -new -key myself.key > myself.csr
+openssl req -new -key myself.key <<EOF > myself.csr
+JP
+Tokyo
+Japari Town
+Japari Company
+Japari Section
+nyanpasu.com
+
+
+EOF
 openssl x509 -days 3650 -req -signkey myself.key < myself.csr > myself.crt
 mkdir -p ssl/development/
 mv myself.crt ssl/development

611d0e41cefafed6342f71580452b87c50e036b7
network <network-team@gmo.jp>
2017-12-13 19:46:27 +0900

    remark interface delte

--- jgob/jgob.go
+++ jgob/jgob.go
@@ -220,26 +220,26 @@ func main() {
 			w.Write([]byte(str))
 		}
 	})
-
-	rtr.HandleFunc("/remark", func(w http.ResponseWriter, r *http.Request) {
-		if checkAuth(r) == false {
-			w.Header().Set("WWW-Authenticate", `Basic realm="JGOB REALM"`)
-			w.WriteHeader(401)
-			w.Write([]byte("401 Unauthorized\n"))
-		} else {
-			RemarkMap["flag"] = true
-			json, err := json.Marshal(RemarkMap)
-			if err != nil {
-				RemarkMap["flag"] = false
-				l.Error(err)
+	/*
+		rtr.HandleFunc("/remark", func(w http.ResponseWriter, r *http.Request) {
+			if checkAuth(r) == false {
+				w.Header().Set("WWW-Authenticate", `Basic realm="JGOB REALM"`)
+				w.WriteHeader(401)
+				w.Write([]byte("401 Unauthorized\n"))
+			} else {
+				RemarkMap["flag"] = true
+				json, err := json.Marshal(RemarkMap)
+				if err != nil {
+					RemarkMap["flag"] = false
+					l.Error(err)
+				}
+				str := fmt.Sprintf("[%s]", string(json))
+				w.Header().Set("Content-Type", "application/json")
+				w.Header().Set("Content-Length", strconv.Itoa(len(str)))
+				w.Write([]byte(str))
 			}
-			str := fmt.Sprintf("[%s]", string(json))
-			w.Header().Set("Content-Type", "application/json")
-			w.Header().Set("Content-Length", strconv.Itoa(len(str)))
-			w.Write([]byte(str))
-		}
-	})
-
+		})
+	*/
 	rtr.HandleFunc("/debug", func(w http.ResponseWriter, r *http.Request) {
 		if checkAuth(r) == false {
 			w.Header().Set("WWW-Authenticate", `Basic realm="JGOB REALM"`)

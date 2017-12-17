91ee16bdf7057b347b83890cae5b9271ba43ea27
Naoya Yokoyama <nnao45@nnao45-pc.local>
2017-12-17 17:11:58 +0900

    maketest

--- jgob/makeTestConfig.sh
+++ jgob/makeTestConfig.sh
@@ -15,3 +15,7 @@ cat << EOT > .env
 USERNAME=user
 PASSWORD=pass
 EOT
+
+cat << EOT > jgob.route
+[{"remark":"kiiiiiiiiiiiiin!!!!!","attrs":{"destination":"111.0.0.0/24","source":"222.0.0.0/24","protocol":"udp","destination-port":"==22222","source-port":"==1111","origin":"e","extcomms":"99999.000000","aspath":"1,2,3,4,5,6,7"}}]
+EOT
